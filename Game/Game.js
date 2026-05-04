50a4157b744c3f5e6617d79a2a4fee1931fe967d
ernestmacharia108-svg <ernestmacharia108@gmail.com>
2026-05-04 18:03:20 +0300

    Update JavaScript to display images for each choice

--- Game/Game.js
+++ Game/Game.js
@@ -1,11 +1,28 @@
+const choiceImages = {
+    rock: 'https://cdn-icons-png.flaticon.com/512/4436/4436481.png',
+    paper: 'https://cdn-icons-png.flaticon.com/512/3442/3442506.png',
+    scissors: 'https://cdn-icons-png.flaticon.com/512/747/747376.png'
+};
+
 function play(playerChoice) {
     const choices = ['rock', 'paper', 'scissors'];
     const compChoice = choices[Math.floor(Math.random() * 3)];
     let res = "";
+    
     if (playerChoice === compChoice) res = "It's a tie!";
     else if ((playerChoice === 'rock' && compChoice === 'scissors') ||
              (playerChoice === 'paper' && compChoice === 'rock') ||
              (playerChoice === 'scissors' && compChoice === 'paper')) res = "You Win!";
     else res = "You Lose!";
-    document.getElementById('result').innerText = `You: ${playerChoice} | AI: ${compChoice} -> ${res}`;
+    
+    // Update player display
+    document.getElementById('playerImg').src = choiceImages[playerChoice];
+    document.getElementById('playerChoice').innerText = playerChoice;
+    
+    // Update AI display
+    document.getElementById('aiImg').src = choiceImages[compChoice];
+    document.getElementById('aiChoice').innerText = compChoice;
+    
+    // Update result
+    document.getElementById('result').innerText = res;
 }
